6d4d5f5d8c028a9b5e8ae518e129a7d2d2722879
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2024-07-14 11:48:37 -0500

    Implement handleDragLeave function

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -18,57 +18,70 @@ const items: DragDropItem[] = [{ name: 'Widget-1' }, { name: 'Widget-2' }, { nam
 
 export const Test = () => {
 	const [dropItems, setDropItems] = useState<DragDropItem[]>([]);
+	const [isOnDragOver, setIsOnDragOver] = useState(false);
 
-	const handleOnDrag = (event: DragEvent, widgetType: string) => {
+	const handleOnDragStart = (event: DragEvent, widgetType: string) => {
 		event.dataTransfer.setData('widgetType', widgetType);
+	};
+
+	const handleDragOver = (event: DragEvent) => {
+		event.preventDefault();
+		setIsOnDragOver(true);
+		console.log('drag over');
+	};
+
+	const handleDragLeave = () => {
+		setIsOnDragOver(false);
+		console.log('drag leave');
 	};
 
 	const handleOnDrop = (event: DragEvent) => {
 		const widget = event.dataTransfer.getData('widgetType');
 		const item = items.find((item) => item.name === widget);
 		if (item) setDropItems([...dropItems, item]);
+		setIsOnDragOver(false);
 
 		console.log('drop', { dropItems });
-	};
-
-	const handleDragOver = (event: DragEvent) => {
-		event.preventDefault();
-		console.log('drag over');
 	};
 
 	return (
 		<>
 			<Container component='main'>
 				<Typography variant='h1'>Test</Typography>
+
 				<Container sx={{ display: 'flex', gap: 2 }}>
 					<Box sx={{ width: '100%' }}>
 						<Stack spacing={2}>
 							{items.map((item, i) => (
-								<Item key={i} draggable onDragStart={(e) => handleOnDrag(e, item.name)}>
+								<Item key={i} draggable onDragStart={(e) => handleOnDragStart(e, item.name)}>
 									{item.name}
 								</Item>
 							))}
 						</Stack>
 					</Box>
 
-					{/* Drop container */}
-					<Box
-						component='div'
-						onDragOver={handleDragOver}
-						onDrop={handleOnDrop}
-						sx={{ width: '100%', border: 1, borderRadius: 1 }}>
-						<Stack spacing={1}>
-							{dropItems.length > 0 ? (
-								dropItems.map((item, i) => (
-									<Item key={i} draggable>
-										{item.name}
-									</Item>
-								))
-							) : (
-								<Typography variant='body1'>Drag and Drop</Typography>
-							)}
-						</Stack>
-					</Box>
+					<Container sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+						{/* Drop container */}
+						<Typography>Question</Typography>
+						<Box
+							component='div'
+							onDragOver={handleDragOver}
+							onDragLeave={handleDragLeave}
+							onDrop={handleOnDrop}
+							sx={{ width: '100%', minHeight: '360px', border: 1, borderRadius: 1, opacity: isOnDragOver ? 0.7 : 1 }}>
+							<Stack spacing={1}>
+								{dropItems.length > 0 ? (
+									dropItems.map((item, i) => (
+										<Item key={i} draggable>
+											{item.name}
+										</Item>
+									))
+								) : (
+									<Typography variant='body1'>Drag and Drop</Typography>
+								)}
+							</Stack>
+						</Box>
+					</Container>
 				</Container>
 
 				<Stack spacing={2} direction='row'>
